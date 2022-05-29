b1fd2f9a2d7896eb21b71355331657fbd8cccfbd
Andre Hehn <a.hehn@web.de>
2022-05-29 18:00:38 +0200

    added winning screen and actual player

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 let fields = [];
 let currentPlayer = 0;
-let possibleWin =[
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6],
-    ]
+let winner;
+let possibleWin = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
 
 //player equal cross ....player odd circle
 function stampSign(id) {
@@ -27,7 +28,10 @@ function crossMove(id) {
     fields[id] = 'cross';
     document.getElementById('cross' + id).classList.remove('d-none');
     checkWin();
-    currentPlayer++;
+    if (!winner) {
+        currentPlayer++;
+        changePlayerToCircle();
+    }
 }
 
 
@@ -35,19 +39,38 @@ function circleMove(id) {
     fields[id] = 'circle';
     document.getElementById('circle' + id).classList.remove('d-none');
     checkWin();
-    currentPlayer++;
+    if (!winner) {
+        currentPlayer++;
+        changePlayerToCross();
+    }
+
+}
+
+
+function changePlayerToCross() {
+    document.getElementById('player').classList.remove("circle");
+    document.getElementById('player').classList.add("cross");
+    document.getElementById('player').src = "./img/cross.png";
+}
+
+
+function changePlayerToCircle() {
+    document.getElementById('player').classList.remove("cross");
+    document.getElementById('player').classList.add("circle");
+    document.getElementById('player').src = "./img/circle.png";
 }
 
 
 function checkWin() {
-    let winner;
-    let winning= false;
+
+    let winning = false;
     for (let i = 0; i < possibleWin.length; i++) {
         const element = possibleWin[i];
-        if (fields[element[0]] == fields[element[1]] && fields[element[1]] == fields[element[2]]) { winner = fields[element[0]]; winning = true;}
+        if (fields[element[0]] == fields[element[1]] && fields[element[1]] == fields[element[2]]) { winner = fields[element[0]]; winning = true; }
     }
-    if(currentPlayer==8 && !winning){ winner ='draw';}
+    if (currentPlayer == 8 && !winning) { winner = 'draw'; }
     showWinner(winner);
+    return winner;
 };
 
 
@@ -55,12 +78,35 @@ function showWinner(winner) {
     if (winner) {
         makeUnclickable();
         console.log(winner);
+        renderHtmlForShowWinner(winner);
+        renderHtmlButtonsForShowWinner();
     }
 }
 
+function renderHtmlButtonsForShowWinner(){
+document.getElementById('buttons').innerHTML=``;
+//needs to be filled
+}
+
+function renderHtmlForShowWinner(winner){
+    if (winner == 'draw') {
+        document.getElementById('header').innerHTML = `<img class ="cross" src="./img/cross.png"><img class ="circle" src="./img/circle.png"><h1 class="h1-right">DRAW</h1>`;
+    }
+    else if (winner == 'circle') {
+        document.getElementById('header').innerHTML = `<img class ="circle" src="./img/circle.png"><h1 class="h1-right">WINS</h1>`;
+    }
+    else if (winner == 'cross') {
+        document.getElementById('header').innerHTML = `<img class ="cross" src="./img/cross.png"><h1 class="h1-right">WINS</h1>`;
+    }
+
+}
 
 function makeUnclickable() {
     for (let i = 0; i < 9; i++) {
         document.getElementById('circle' + i).parentNode.setAttribute('onclick', '');
     }
 }
+
+//functions 4 buttons
+
+//functions for winningline
